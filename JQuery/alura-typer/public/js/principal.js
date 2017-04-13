ba1cc55297de44d15218cc2c9ae787fb63331036
Manuel Cordeiro Neto <manuel.cdr@gmail.com>
2017-04-13 16:41:34 -0300

    adicionado verificação da frase e alguns estilos

--- JQuery/alura-typer/public/js/principal.js
+++ JQuery/alura-typer/public/js/principal.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     inicializaTempo();
     inicializaContadores();
     inicializaReiniciar();
+    inicializaVerificacaoDigitacao();
 });
 
 // document.onload -------------------------------------------------------------------------------------
@@ -19,14 +20,36 @@ var contadorCaracteres = $("#contador-caracteres");
 var contadorPalavras = $("#contador-palavras");
 var contadorPontos = $("#contador-pontos");
 var botaoReiniciar = $("#botao-reiniciar");
+var frase = $(".frase");
 
 var tempoInicial = tempoDigitacao.text();
+var idInterval = 0;
 
 //#endregion propriedades -------------------------------------------------------------------------------------
 
 
 
 //#region funções ---------------------------------------------------------------------------------------------
+
+function verificaDigitacao(event) {
+    var digitado = campo.val();
+    var fraseCortada = frase.text().substr(0, digitado.length);
+    console.log(event);
+    if (digitado == fraseCortada) {
+        console.log("certo");
+        campo.addClass("digitado-certo");
+        campo.removeClass("digitado-errado");
+    } else {
+        console.log("errado");
+        campo.addClass("digitado-errado");
+        campo.removeClass("digitado-certo");
+        event.preventDefault();
+    }
+}
+
+function inicializaVerificacaoDigitacao() {
+    campo.on("keypress", verificaDigitacao);
+}
 
 function inicializaTempo() {
     campo.val("");
@@ -44,7 +67,7 @@ function inicializaReiniciar() {
 function iniciarTempo() {
     tempo = tempoInicial;
     
-    var idInterval = setInterval(function() {
+    idInterval = setInterval(function() {
         tempo--
         tempoDigitacao.text(tempo);
         if (tempo < 1) {
@@ -69,10 +92,13 @@ function atualizarContadores() {
 function reiniciarJogo() {
     campo.val("");
     campo.attr("disabled", false);
+    campo.removeClass("digitado-certo");
+    campo.removeClass("digitado-errado");
     tempoDigitacao.text(tempoInicial);
     contadorCaracteres.text("0");
     contadorPalavras.text("0");
     contadorPontos.text("0");
+    clearInterval(idInterval);
     inicializaTempo();
 }
 
